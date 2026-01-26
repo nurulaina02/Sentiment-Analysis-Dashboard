70251d0acd73454f164abadaadddb143dbfa9ed7
nurulaina02 <ainasaharani24@gmail.com>
2026-01-27 03:18:47 +0800

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -2,7 +2,6 @@ import re
 
 def clean_text(text):
     text = text.lower()
-    text = re.sub(r"http\S+", "", text)      # remove URLs
-    text = re.sub(r"[^a-zA-Z\s]", "", text)  # remove symbols
-    text = re.sub(r"\s+", " ", text).strip()
-    return text
+    text = re.sub(r"http\S+", "", text)
+    text = re.sub(r"[^a-zA-Z\s]", "", text)
+    return text.strip()
