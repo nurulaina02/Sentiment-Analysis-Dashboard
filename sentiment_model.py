b54ae26e09e78911c01c043b1ae677eb373ee6a5
nurulaina02 <ainasaharani24@gmail.com>
2026-01-27 03:05:33 +0800

    Update sentiment_model.py

--- sentiment_model.py
+++ sentiment_model.py
@@ -1,6 +1,5 @@
 from transformers import pipeline
 
-# Load sentiment analysis pipeline
 sentiment_pipeline = pipeline(
     "sentiment-analysis",
     model="distilbert-base-uncased-finetuned-sst-2-english"
