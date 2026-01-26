26aad96f5cb2b38dc5eb559586dc8deef81b0f40
nurulaina02 <ainasaharani24@gmail.com>
2026-01-27 03:23:05 +0800

    Update and rename sentiment_model.py to sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,11 +1,11 @@
 from transformers import pipeline
 
-# Load pre-trained sentiment analysis model
-sentiment_pipeline = pipeline(
+# Load model once (important for Streamlit performance)
+sentiment_model = pipeline(
     "sentiment-analysis",
     model="distilbert-base-uncased-finetuned-sst-2-english"
 )
 
 def predict_sentiment(text):
-    result = sentiment_pipeline(text)[0]
+    result = sentiment_model(text)[0]
     return result["label"], result["score"]
