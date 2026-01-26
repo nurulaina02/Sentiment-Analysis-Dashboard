40a9462fce572722bf30e87bcacbece9e48273c0
nurulaina02 <ainasaharani24@gmail.com>
2026-01-27 03:22:23 +0800

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,7 @@
 import re
 
 def clean_text(text):
-    text = text.lower()
+    text = str(text).lower()
     text = re.sub(r"http\S+", "", text)
     text = re.sub(r"[^a-zA-Z\s]", "", text)
     return text.strip()
